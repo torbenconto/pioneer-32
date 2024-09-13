4fd61785843c15fca4004cf8f5b69d921cf1f73f
torbenconto <torbenmconto@gmail.com>
2024-09-13 11:38:40 -0400

    drive func, direction enum

--- src/lib/motor/motor.cpp
+++ src/lib/motor/motor.cpp
@@ -1,1 +1,10 @@
 #include "motor.h"
+
+/*
+  This Drives a motor in a specific direction, at a specified speed:
+    - turnDirection: member of "direction" enum ---> direction.clockwise or direction.counterclockwise
+    - speed: 0 to 255 ---> 0 = stop / 255 = fast
+*/
+void Motor::drive(direction turnDirection, int speed) {
+    
+}
--- src/lib/motor/motor.h
+++ src/lib/motor/motor.h
@@ -6,6 +6,13 @@
 class Motor {
     public: 
         Motor(GPIOPin *pin) : pin(pin) {};
+
+        enum direction {
+            clockwise,
+            counterclockwise
+        };
+        
+        void drive(direction d, int speed);
     private:
         GPIOPin *pin;
 
