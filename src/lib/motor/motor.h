07625b8c4b8ce51eb21e16da67a66e783f9512d1
torbenconto <torbenmconto@gmail.com>
2024-09-13 11:27:22 -0400

    base motor class

--- src/lib/motor/motor.h
+++ src/lib/motor/motor.h
@@ -1,5 +1,14 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+#include "gpio.h"
+
+class Motor {
+    public: 
+        Motor(GPIOPin *pin) : pin(pin) {};
+    private:
+        GPIOPin *pin;
+
+}
 
 #endif // MOTOR_H
