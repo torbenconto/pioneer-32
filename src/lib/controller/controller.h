e90832c640dd70c13f9a7f72e3c1d68ce585ef64
torbenconto <torbenmconto@gmail.com>
2024-09-16 20:36:09 -0400

    xbox controller support

---
+++ src/lib/controller/controller.h
@@ -0,0 +1,236 @@
+// Jacked from https://github.com/asukiaaa
+#pragma once
+
+#include <NimBLEDevice.h>
+#include <XboxControllerNotificationParser.h>
+#include "report_builder.h"
+
+static NimBLEUUID uuidServiceHid("1812");
+static NimBLEUUID uuidCharaReport("2a4d");
+static NimBLEUUID uuidServiceBattery("180f");
+
+static NimBLEAdvertisedDevice* advDevice;
+static NimBLEClient* pConnectedClient = nullptr;
+
+static const uint16_t controllerAppearance = 964;
+
+enum class ConnectionState : uint8_t {
+  Connected = 0,
+  WaitingForFirstNotification = 1,
+  Found = 2,
+  Scanning = 3,
+};
+
+class ClientCallbacks : public NimBLEClientCallbacks {
+ public:
+  ConnectionState* pConnectionState;
+  ClientCallbacks(ConnectionState* pConnectionState) : pConnectionState(pConnectionState) {}
+
+  void onConnect(NimBLEClient* pClient) override {
+    *pConnectionState = ConnectionState::WaitingForFirstNotification;
+  }
+
+  void onDisconnect(NimBLEClient* pClient) override {
+    *pConnectionState = ConnectionState::Scanning;
+    pConnectedClient = nullptr;
+  }
+
+  uint32_t onPassKeyRequest() override { return 0; }
+  bool onConfirmPIN(uint32_t pass_key) override { return true; }
+  void onAuthenticationComplete(ble_gap_conn_desc* desc) override {
+    if (!desc->sec_state.encrypted) {
+      NimBLEDevice::getClientByID(desc->conn_handle)->disconnect();
+    }
+  }
+};
+
+class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks {
+ public:
+  AdvertisedDeviceCallbacks(String strTargetDeviceAddress, ConnectionState* pConnectionState)
+      : pConnectionState(pConnectionState) {
+    if (!strTargetDeviceAddress.isEmpty()) {
+      targetDeviceAddress = new NimBLEAddress(strTargetDeviceAddress.c_str());
+    }
+  }
+
+ private:
+  NimBLEAddress* targetDeviceAddress = nullptr;
+  ConnectionState* pConnectionState;
+
+  void onResult(NimBLEAdvertisedDevice* advertisedDevice) override {
+    if ((targetDeviceAddress != nullptr && advertisedDevice->getAddress().equals(*targetDeviceAddress)) ||
+        (targetDeviceAddress == nullptr &&
+         advertisedDevice->getAppearance() == controllerAppearance &&
+         advertisedDevice->getServiceUUID().equals(uuidServiceHid))) {
+      *pConnectionState = ConnectionState::Found;
+      advDevice = advertisedDevice;
+    }
+  }
+};
+
+class Controller {
+ public:
+  Controller(String targetDeviceAddress = "") {
+    advDeviceCBs = new AdvertisedDeviceCallbacks(targetDeviceAddress, &connectionState);
+    clientCBs = new ClientCallbacks(&connectionState);
+  }
+
+  AdvertisedDeviceCallbacks* advDeviceCBs;
+  ClientCallbacks* clientCBs;
+  uint8_t battery = 0;
+  static const int deviceAddressLen = 6;
+  uint8_t deviceAddressArr[deviceAddressLen];
+
+  void begin() {
+    NimBLEDevice::init("");
+    NimBLEDevice::setOwnAddrType(BLE_OWN_ADDR_PUBLIC);
+    NimBLEDevice::setSecurityAuth(true, false, false);
+  }
+
+  void writeHIDReport(uint8_t* dataArr, size_t dataLen) {
+    if (pConnectedClient == nullptr) return;
+    NimBLEClient* pClient = pConnectedClient;
+    auto pService = pClient->getService(uuidServiceHid);
+    for (auto pChara : *pService->getCharacteristics()) {
+      if (pChara->canWrite()) {
+        pChara->writeValue(dataArr, dataLen, false);
+      }
+    }
+  }
+
+  void writeHIDReport(const ReportBase& repo) {
+    writeHIDReport((uint8_t*)repo.arr8t, repo.arr8tLen);
+  }
+
+  void writeHIDReport(const ReportBeforeUnion& repoBeforeUnion) {
+    ReportBase repo;
+    repo.v = repoBeforeUnion;
+    writeHIDReport((uint8_t*)repo.arr8t, repo.arr8tLen);
+  }
+
+  void onLoop() {
+    if (!isConnected()) {
+      if (advDevice != nullptr) {
+        auto connectionResult = connectToServer(advDevice);
+        if (!connectionResult || !isConnected()) {
+          NimBLEDevice::deleteBond(advDevice->getAddress());
+          connectionState = ConnectionState::Scanning;
+        } else {
+          connectionState = ConnectionState::Connected;
+        }
+        advDevice = nullptr;
+      } else if (!isScanning()) {
+        startScan();
+      }
+    }
+  }
+
+  String buildDeviceAddressStr() {
+    char buffer[18];
+    snprintf(buffer, sizeof(buffer), "%02x:%02x:%02x:%02x:%02x:%02x",
+             deviceAddressArr[5], deviceAddressArr[4], deviceAddressArr[3],
+             deviceAddressArr[2], deviceAddressArr[1], deviceAddressArr[0]);
+    return String(buffer);
+  }
+
+  void startScan() {
+    connectionState = ConnectionState::Scanning;
+    auto pScan = NimBLEDevice::getScan();
+    pScan->setDuplicateFilter(false);
+    pScan->setAdvertisedDeviceCallbacks(advDeviceCBs);
+    pScan->setInterval(97);
+    pScan->setWindow(97);
+    pScan->start(scanTime, &Controller::scanCompleteCB, false);
+  }
+
+  XboxControllerNotificationParser xboxNotif;
+
+  bool isWaitingForFirstNotification() {
+    return connectionState == ConnectionState::WaitingForFirstNotification;
+  }
+  bool isConnected() {
+    return connectionState == ConnectionState::WaitingForFirstNotification ||
+           connectionState == ConnectionState::Connected;
+  }
+  uint8_t getCountFailedConnection() { return countFailedConnection; }
+
+ private:
+  ConnectionState connectionState = ConnectionState::Scanning;
+  uint32_t scanTime = 4;
+  uint8_t countFailedConnection = 0;
+  uint8_t retryCountInOneConnection = 3;
+  unsigned long retryIntervalMs = 100;
+
+  bool isScanning() { return NimBLEDevice::getScan()->isScanning(); }
+
+  bool connectToServer(NimBLEAdvertisedDevice* advDevice) {
+    NimBLEClient* pClient = NimBLEDevice::getClientByPeerAddress(advDevice->getAddress());
+    if (!pClient) {
+      if (NimBLEDevice::getClientListSize() >= NIMBLE_MAX_CONNECTIONS) {
+        return false;
+      }
+      pClient = NimBLEDevice::createClient();
+      pClient->setClientCallbacks(clientCBs, true);
+      pClient->connect(advDevice, true);
+    }
+
+    int retryCount = retryCountInOneConnection;
+    while (!pClient->isConnected()) {
+      if (retryCount <= 0) {
+        return false;
+      }
+      delay(retryIntervalMs);
+      pClient->connect(true);
+      --retryCount;
+    }
+
+    bool result = afterConnect(pClient);
+    if (!result) {
+      return result;
+    }
+    pConnectedClient = pClient;
+    return true;
+  }
+
+  bool afterConnect(NimBLEClient* pClient) {
+    memcpy(deviceAddressArr, pClient->getPeerAddress().getNative(), deviceAddressLen);
+    for (auto pService : *pClient->getServices(true)) {
+      auto sUuid = pService->getUUID();
+      if (sUuid.equals(uuidServiceHid)) {
+        for (auto pChara : *pService->getCharacteristics(true)) {
+          charaHandle(pChara);
+          charaSubscribeNotification(pChara);
+        }
+      }
+    }
+    return true;
+  }
+
+  void charaHandle(NimBLERemoteCharacteristic* pChara) {
+    if (pChara->canRead()) {
+      auto str = pChara->readValue();
+    }
+  }
+
+  void charaSubscribeNotification(NimBLERemoteCharacteristic* pChara) {
+    if (pChara->canNotify()) {
+      if (pChara->subscribe(true, std::bind(&Controller::notifyCB, this, std::placeholders::_1,
+                                             std::placeholders::_2, std::placeholders::_3,
+                                             std::placeholders::_4), true)) {
+      }
+    }
+  }
+
+  void notifyCB(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length,
+                bool isNotify) {
+    auto sUuid = pRemoteCharacteristic->getRemoteService()->getUUID();
+    if (sUuid.equals(uuidServiceHid)) {
+      xboxNotif.update(pData, length);
+    } else if (sUuid.equals(uuidServiceBattery)) {
+      battery = pData[0];
+    }
+  }
+
+  static void scanCompleteCB(NimBLEScanResults results) {
+  }
+};
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -2,6 +2,7 @@
 #include "gpio.h"
 #include "motor.h"
 #include "pins.h"
+#include "controller.h"
 
 /*
   Pin initializations, see lib/gpio for GPIOPin class
@@ -36,14 +37,53 @@ GPIOPin p_STBY(STBY, OUTPUT);
 Motor motorA(&p_AIN1, &p_AIN2, &p_PWMA);
 Motor motorB(&p_BIN1, &p_BIN2, &p_PWMB);
 
+// Controller params
+const float JOYSTICK_MIDDLE = 0.5;
+const float DEAD_ZONE = 0.05; 
+
+// Controller instance
+Controller xboxController("f4:6a:d7:12:31:be");
+
 void setup() {
   // Disable standby
   p_STBY.writeDigital(HIGH);
+
+  xboxController.begin();
 }
 
 void loop() {
-  motorA.drive(clockwise, 255);
-  delay(1000);
-  motorA.drive(clockwise, 0);
-  delay(1000);
+  xboxController.onLoop();
+
+  if (xboxController.isConnected()) {
+    if (xboxController.isWaitingForFirstNotification()) {
+      uint16_t joystickMax = XboxControllerNotificationParser::maxJoy; // Maximum possible joystick value
+      float joyLVert = (float)xboxController.xboxNotif.joyLVert / joystickMax; // Normalize joystick vertical position to [0, 1]
+
+      if (joyLVert > JOYSTICK_MIDDLE + DEAD_ZONE) {
+          // Joystick is pushed beyond the positive dead zone
+          // Map joystick value to the motor speed range
+          // joyLVert - (JOYSTICK_MIDDLE + DEAD_ZONE) calculates how far the joystick is beyond the positive dead zone
+          // 1.0 - JOYSTICK_MIDDLE - DEAD_ZONE is the maximum range where the joystick can still affect the motor
+          float speed = (joyLVert - (JOYSTICK_MIDDLE + DEAD_ZONE)) / (1.0 - JOYSTICK_MIDDLE - DEAD_ZONE);
+          // Scale speed to motor range (0 to 255)
+          motorA.drive(clockwise, 255 * speed);
+      } else if (joyLVert < JOYSTICK_MIDDLE - DEAD_ZONE) {
+          // Joystick is pushed beyond the negative dead zone
+          // Map joystick value to the motor speed range
+          // (JOYSTICK_MIDDLE - DEAD_ZONE) - joyLVert calculates how far the joystick is below the negative dead zone
+          // JOYSTICK_MIDDLE - DEAD_ZONE is the maximum range where the joystick can still affect the motor
+          float speed = ((JOYSTICK_MIDDLE - DEAD_ZONE) - joyLVert) / (JOYSTICK_MIDDLE - DEAD_ZONE);
+          // Scale speed to motor range (0 to 255)
+          motorA.drive(counterclockwise, 255 * speed);
+      } else {
+          // Joystick is within the dead zone
+          // No significant movement detected, so brake the motor
+          motorA.brake();
+      }
+    }
+  } else {
+    if (xboxController.getCountFailedConnection() > 2) {
+      ESP.restart();
+    }
+  }
 }
